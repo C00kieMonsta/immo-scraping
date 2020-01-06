a2d719eff2953b27f80dc890126d5444e36a5414
Antoine <karagan.app@gmail.com>
2020-01-06 14:22:17 +0100

    fix

--- immo-web/scrap-immoweb.py
+++ immo-web/scrap-immoweb.py
@@ -12,40 +12,75 @@ class Asset:
         self.location = location
         self.photos = photos
 
-# make request to desired website
-# <div id="results" ...
-#   <div id="id_of_item"
-#       <a title="small_descriptio" href="link_to_asset"
-result = requests.get("https://www.immoweb.be/fr/recherche/maison/a-vendre/bruxelles/1000")
+def extractAndAppend(soup):
+    # #########################################
+    #       1# Extract Info
+    # #########################################
+    items = soup.findAll("div", {"class": "result-xl"})
 
-# storing content of request
-src = result.content
+    # #########################################
+    #       2# Feed CSV with first page
+    # #########################################
+    with open('csv/immoweb.csv', 'a', newline='', encoding="utf-8") as csvfile:
 
-# BS Object
-soup = BeautifulSoup(src, "lxml")
-results = soup.find(id='result')
-items = soup.findAll("div", {"class": "result-xl"})
+        filewriter = csv.writer(csvfile)
 
-# list of assets
-assets = []
+        for item in items:
+            id = item.attrs['id']
+            a_tag = item.find('a')
+            url = a_tag.attrs['href']
+            donnees = a_tag.find("div", {"class": "xl-donnees-bien"})
+            price = re.sub('\\s\\s+', ' ', donnees.find("div", {"class": ["xl-price", "rangePrice"]}).text)
+            size = re.sub('\\s\\s+', ' ', donnees.find("div", {"class": ["xl-surface-ch"]}).text)
+            desc = re.sub('\\s\\s+', ' ', donnees.find("div", {"class": ["xl-desc"]}).text)
+            title = re.sub('\\s\\s+', ' ', a_tag.find("div", {"class": "title-bar-left"}).text)
+            location = re.sub('\\s\\s+', ' ', a_tag.find("div", {"class": "title-bar-right"}).text)
 
+            if a_tag.find("div", {"class": "xl-photo-bien owl-carousel"}):
+                photo = a_tag.find("div", {"class": "xl-photo-bien owl-carousel"}).find("img").attrs['data-src']
+            else:
+                photo = ''
+
+            # write to csv
+            filewriter.writerow([id, url, price, size, title, desc, location, photo])
 
 with open('csv/immoweb.csv', 'w') as csvfile:
     
-    filewriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    filewriter.writerow(['ID', 'Url', 'Description', 'Title', 'Location'])
+    filewriter = csv.writer(csvfile, delimiter=',', quotechar='|')
+    filewriter.writerow(['Id', 'Url', 'Price', 'Size', 'Title', 'Description', 'Location', 'Photo'])
 
-    for item in items:
-        id = item.attrs['id']
-        a_tag = item.find('a')
-        url = a_tag.attrs['href']
-        desc = a_tag.attrs['title']
-        title = re.sub('\s\s+', ' ', a_tag.find("div", {"class": "title-bar-left"}).text)
-        location = re.sub('\s\s+', ' ', a_tag.find("div", {"class": "title-bar-right"}).text)
-        photos = map(lambda x: x.attrs['data-src'] , a_tag.find("div", {"class": "xl-photo-bien owl-carousel"}).findAll("img"))
-        
-        # write to csv
-        filewriter.writerow([id, url, title, location])
+codes_communes = [1000, 1030, 1040, 1050, 1060, 1070, 1080, 1081, 1082, 1083, 1090, 1140, 1150, 1160, 1170, 1180, 1190, 1200, 1210]
 
-        # asset = Asset(id, url, desc, title, location, list(photos))
-        # assets.append(asset)
+for code in codes_communes:
+
+    # #########################################
+    #       1# Request page for url
+    # #########################################
+    url = "https://www.immoweb.be/fr/recherche/maison/a-vendre/bruxelles/" + str(code)
+    result = requests.get(url)
+    src = result.content
+
+    # #########################################
+    #       2# Soup Object
+    # #########################################
+    print("log info -- extract for commune", str(code), "page 1")
+    soup = BeautifulSoup(src, "lxml")
+
+    # #########################################
+    #       3# Extract Info
+    # #########################################
+    numbers = soup.select_one("ul.nav-nummer")
+    extractAndAppend(soup)
+
+    # #########################################
+    #       4# Feed CSV with additional pages
+    # #########################################    
+    if numbers:
+        number_of_pages = list(map(lambda y: y.text, filter(lambda x: x.find("a"), numbers.findAll("li"))))
+
+        for page_num in number_of_pages:
+            print("log info -- extract for commune", str(code), "page", page_num)
+            requests.get(url + "?page=" + page_num)
+            src = result.content
+            soup = BeautifulSoup(src, "lxml")
+            extractAndAppend(soup)
